1afc0669cffd8c7fcd1770a463bee74edd39519e
Saksham Dhull <sakshamdhull442@gmail.com>
2018-11-11 11:49:22 +0530

    edited

--- receiver.vhd
+++ receiver.vhd
@@ -15,26 +15,24 @@ entity receiver is
 end receiver;
 
 architecture Behavioral of receiver is
-    type state_type is (Idle1, Idle2, s0, s1, s2, s3, s4, s5, s6, s7, stop, start);
+    type state_type is (idles1, idles2, s0, s1, s2, s3, s4, s5, s6, s7, stop, start);
     signal state: state_type;
     signal ctr: std_logic_vector(3 downto 0) := "0000";
-    signal c_data: std_logic_vector(7 downto 0) := "00000000";
-    signal ldctr, inc, z, ld0, ld1, ld2, ld3, ld4, ld5, ld6, ld7: std_logic;
+    signal data: std_logic_vector(7 downto 0) := "00000000";
+    signal ldctr, inc, z, load0, load1, load2, load3, load4, load5, load6, load7: std_logic;
 begin
 
 
     state_change: process(rxclk, reset) 
-        variable my_line: line;
-        begin
-        if (reset = '1') then state <= Idle1; end if;
-        
+        begin 
+        if (reset = '1') then state <= idles1; end if;
         if (rising_edge(rxclk) and rx_en = '1') then
             case state is
-                when Idle1 => 
+                when idles1 => 
                     if rx_in = '0' then state <= start; end if;
-                when Idle2 =>
+                when idles2 =>
                     if rx_in = '0' then state <= start; 
-                    elsif uld_rx = '1' then state <= Idle1; end if;
+                    elsif uld_rx = '1' then state <= idles1; end if;
                 when start =>
                     if z = '1' then state <= s0; end if;
                 when s0 =>
@@ -54,47 +52,47 @@ begin
                 when s7 => 
                     if z = '1' then state <= stop; end if;
                 when stop =>
-                    if z = '1' then state <= Idle2; end if;
+                    if z = '1' then state <= idles2; end if;
             end case;
         end if;
     end process;
     
     control_outputs: process(z, state, rx_en) begin
-        ldctr <= '0'; ld0 <= '0'; ld1 <= '0'; ld2 <= '0'; ld3 <= '0';
-        ld4 <= '0'; ld5 <= '0'; ld6 <= '0'; ld7 <= '0'; rx_empty <= '1';
+        ldctr <= '0'; load0 <= '0'; load1 <= '0'; load2 <= '0'; load3 <= '0';
+        load4 <= '0'; load5 <= '0'; load6 <= '0'; load7 <= '0'; rx_empty <= '1';
         inc <= '0';
         case state is
-            when Idle1 => 
+            when idles1 => 
                 rx_empty <= '1';
                 if (rx_in = '0') then ldctr <= '1'; end if;
-            when Idle2 => 
+            when idles2 => 
                 rx_empty <= '0';
                 if (rx_in = '0') then ldctr <= '1'; end if;
             when start => inc <= '1';
             when s0 =>
                 inc <= '1';
-                if (z = '1') then ld0 <= '1'; end if;
+                if (z = '1') then load0 <= '1'; end if;
             when s1 =>
                 inc <= '1';
-                if (z = '1') then ld1 <= '1'; end if;
+                if (z = '1') then load1 <= '1'; end if;
             when s2 =>
                 inc <= '1';
-                if (z = '1') then ld2 <= '1'; end if;
+                if (z = '1') then load2 <= '1'; end if;
             when s3 =>
                 inc <= '1';
-                if (z = '1') then ld3 <= '1'; end if;
+                if (z = '1') then load3 <= '1'; end if;
             when s4 =>
                 inc <= '1';
-                if (z = '1') then ld4 <= '1'; end if;
+                if (z = '1') then load4 <= '1'; end if;
             when s5 =>
                 inc <= '1';
-                if (z = '1') then ld5 <= '1'; end if;
+                if (z = '1') then load5 <= '1'; end if;
             when s6 =>
                 inc <= '1';
-                if (z = '1') then ld6 <= '1'; end if;
+                if (z = '1') then load6 <= '1'; end if;
             when s7 =>
                 inc <= '1';
-                if (z = '1') then ld7 <= '1'; end if;
+                if (z = '1') then load7 <= '1'; end if;
             when stop =>
                 inc <= '1';
                 if (z = '1') then rx_empty <= '0'; end if;    
@@ -102,28 +100,28 @@ begin
     end process;
     
     -- Datapath circuit
-    clock_datapath: process(rxclk, reset) begin
+    datapath: process(rxclk, reset) begin
         if (reset = '1') then
-            ctr <= "0000"; c_data <= "00000000";
+            ctr <= "0000"; data <= "00000000";
         end if;
         
         if (rising_edge(rxclk)) then 
             if (inc = '1') then ctr <= ctr + 1; end if;
             if (ldctr = '1') then ctr <= "0000"; end if;
             if (inc = '1') then ctr <= ctr + 1; end if;
-            if (ld0 = '1') then c_data(0) <= rx_in; end if; 
-            if (ld1 = '1') then c_data(1) <= rx_in; end if;
-            if (ld2 = '1') then c_data(2) <= rx_in; end if;
-            if (ld3 = '1') then c_data(3) <= rx_in; end if;
-            if (ld4 = '1') then c_data(4) <= rx_in; end if;
-            if (ld5 = '1') then c_data(5) <= rx_in; end if;
-            if (ld6 = '1') then c_data(6) <= rx_in; end if;
-            if (ld7 = '1') then c_data(7) <= rx_in; end if;
-            if (uld_rx) = '1' then rx_data <= c_data; end if;
+            if (load0 = '1') then data(0) <= rx_in; end if; 
+            if (load1 = '1') then data(1) <= rx_in; end if;
+            if (load2 = '1') then data(2) <= rx_in; end if;
+            if (load3 = '1') then data(3) <= rx_in; end if;
+            if (load4 = '1') then data(4) <= rx_in; end if;
+            if (load5 = '1') then data(5) <= rx_in; end if;
+            if (load6 = '1') then data(6) <= rx_in; end if;
+            if (load7 = '1') then data(7) <= rx_in; end if;
+            if (uld_rx) = '1' then rx_data <= data; end if;
         end if;
     end process;
     
-    signals_datapath: process(c_data, ctr) begin
+    signals_datapath: process(data, ctr) begin
         if (ctr = "0110") then z <= '1'; else z <= '0'; end if; 
     end process;
 end Behavioral;
--- transmitter.vhd
+++ transmitter.vhd
@@ -1,37 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 09/27/2018 01:47:11 PM
--- Design Name: 
--- Module Name: receiver - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity transmitter is
     Port (tx_out, tx_empty: out std_logic; 
@@ -41,14 +11,9 @@ entity transmitter is
 end transmitter;
 
 architecture Behavioral of transmitter is
-    --Stores the loaded data in the transmitter
-    signal c_data: std_logic_vector(7 downto 0) := "00000000";
-    -- Stores the states of FSM
+    signal data: std_logic_vector(7 downto 0) := "00000000";
     type state_type IS (Idle, start, s0, s1, s2, s3, s4, s5, s6, s7);
     signal state: state_type;
-    
---    -- Make a temporary object tx_empty which is 0 by default
---    signal qtx_empty: std_logic := '1';
 begin
     state_changes: process(reset, txclk) begin
         if (reset = '1') then state <= Idle; end if;
@@ -76,21 +41,21 @@ begin
             when start =>
                 tx_out <= '0'; tx_empty <= '0';
             when s0 =>        
-                tx_out <= c_data(0); tx_empty <= '0';
+                tx_out <= data(0); tx_empty <= '0';
             when s1 =>
-                tx_out <= c_data(1); tx_empty <= '0';
+                tx_out <= data(1); tx_empty <= '0';
             when s2 =>
-                tx_out <= c_data(2); tx_empty <= '0';
+                tx_out <= data(2); tx_empty <= '0';
             when s3 =>
-                tx_out <= c_data(3); tx_empty <= '0';
+                tx_out <= data(3); tx_empty <= '0';
             when s4 =>
-                tx_out <= c_data(4); tx_empty <= '0';
+                tx_out <= data(4); tx_empty <= '0';
             when s5 => 
-                tx_out <= c_data(5); tx_empty <= '0';
+                tx_out <= data(5); tx_empty <= '0';
             when s6 =>
-                tx_out <= c_data(6); tx_empty <= '0';
+                tx_out <= data(6); tx_empty <= '0';
             when s7 =>
-                tx_out <= c_data(7); tx_empty <= '0';
+                tx_out <= data(7); tx_empty <= '0';
         end case;
     end process;
     
@@ -98,7 +63,7 @@ begin
     Load_data: process(txclk) begin
         if (rising_edge(txclk)) then 
             if (ld_tx = '1') then
-                c_data <= tx_data;
+                data <= tx_data;
             end if;
         end if;
     end process;
